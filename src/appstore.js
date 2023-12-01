2968828daaf106d15a7d66bb3b3c6c4769eb988d
yap cheah shen <yapcheahshen@gmail.com>
2023-12-01 10:53:00 +0800

    before lb

---
+++ src/appstore.js
@@ -0,0 +1,19 @@
+export * from 'accelon23/src/store.js';
+import {ACC23 as _ACC23} from 'accelon23/src/appconst.js'
+
+_ACC23.CacheName='v1::lokto',
+_ACC23.AppVer='23.10.31';
+_ACC23.AppName='lokto'
+_ACC23.AppTitle='我的應用'
+_ACC23.allptks=['lokto','lokto-en',"lokto-ru"];
+_ACC23.selectedptks=['lokto','lokto-en']
+_ACC23.activeptk='lokto'
+_ACC23.ptkinfo={
+    'lokto':"中文版",
+    'lokto-en':"英文版",
+    'lokto-ru':"俄文版",
+}
+_ACC23.favorites=[];
+_ACC23.MAXFAVORITE=100;
+_ACC23.ITEMPERPAGE=5;
+export const ACC23 = _ACC23;
