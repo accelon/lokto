f9f17445f2bce974eef28ccd9647dc5b5c9e1758
yap cheah shen <yapcheahshen@gmail.com>
2023-12-01 19:30:20 +0800

    add japanese

--- build-data.cmd
+++ build-data.cmd
@@ -1,3 +1,4 @@
 cmd/c ptk ptk 
 cmd/c ptk ptk  lokto-en
 cmd/c ptk ptk  lokto-ru
+cmd/c ptk ptk  lokto-jp
--- src/appstore.js
+++ src/appstore.js
@@ -4,14 +4,15 @@ import {ACC23 as _ACC23} from 'accelon23/src/appconst.js'
 _ACC23.CacheName='v1::lokto',
 _ACC23.AppVer='23.12.1';
 _ACC23.AppName='lokto'
-_ACC23.AppTitle='我的應用'
-_ACC23.allptks=['lokto','lokto-en','lokto-ru'];
-_ACC23.selectedptks=['lokto','lokto-en','lokto-ru'];
+_ACC23.AppTitle='樂渡法師'
+_ACC23.allptks=['lokto','lokto-en','lokto-ru','lokto-jp'];
+_ACC23.selectedptks=['lokto','lokto-en','lokto-jp','lokto-ru'];
 _ACC23.activeptk='lokto'
 _ACC23.ptkinfo={
     'lokto':"中文版",
     'lokto-en':"英文版",
     'lokto-ru':"俄文版",
+    'lokto-jp':"日文版",
 }
 _ACC23.favorites=[];
 _ACC23.MAXFAVORITE=100;
